389f96c4b3b8348515e3d308bbc37d0b5dcd9cb3
Say0o <renanmv10@hotmail.com>
2023-11-22 17:25:19 -0300

    ajustes finos

--- src/main/java/Concecionaria/repository/FuncionarioRepository.java
+++ src/main/java/Concecionaria/repository/FuncionarioRepository.java
@@ -1,9 +1,11 @@
 package Concecionaria.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import Concecionaria.entities.Funcionario;
 
+@Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 
 }
